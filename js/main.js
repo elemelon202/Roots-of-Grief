f81f50ec41ceb83b3810af6d1a5792606a1c3923
S Bulman <sm.bulman@gmail.com>
2024-05-27 11:25:22 +0900

    Building the bio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("moreBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) => 
 item.addEventListener("click", function() {

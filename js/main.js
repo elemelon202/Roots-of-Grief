84c3e0f5b441e647f992c8c3ad225ddfe1da58c3
S Bulman <sm.bulman@gmail.com>
2024-05-27 10:49:06 +0900

    Creating the tour section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ item.addEventListener("click", function() {
     this.classList.toggle("collapsible--expanded");
 })
 );
+
+const badge = document.querySelectorAll(".tour__badge");
+badge.forEach((item) => 
+item.addEventListener("click", function() {
+    this.classList.toggle("badge--secondary");
+})
+);
